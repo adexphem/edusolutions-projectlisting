4e79e1ab035c15e766f23e47e4d29f73ac1c6c3d
adexphem <adexphem4real@gmail.com>
2019-11-24 22:28:29 +0100

    updates

--- src/base.js
+++ src/base.js
@@ -1,9 +1,9 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: process.env.REACT_APP_API_KEY || "",
-  authDomain: process.env.REACT_APP_AUTHDOMAIN || "",
-  databaseURL: process.env.REACT_APP_DATABASEURL || ""
+  apiKey: process.env.REACT_APP_apiKey || "AIzaSyDQZIZ8-irOukHmGxVBLZ9x8_K1NZIugbo",
+  authDomain: process.env.REACT_APP_authDomain || "edusolutions-2086f.firebaseapp.com",
+  databaseURL: process.env.REACT_APP_databaseURL || "https://edusolutions-2086f.firebaseio.com"
 };
 
 firebase.initializeApp(config);
