33176e0edfbebd94af510c0fc3a952d427242b20
adexphem <adexphem4real@gmail.com>
2019-11-24 21:08:49 +0100

    update readme

--- src/App.js
+++ src/App.js
@@ -1,26 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import { projectsRef } from './base'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+export default class App extends Component {
+  state = {
+    title: '',
+    url: '',
+    projectList: []
+  }
+
+  componentDidMount() {
+    this.getProjects();
+  }
+
+  handleChange = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    this.setState({[name]: value});
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const {url, title} = this.state;
+    const data = {
+      title,
+      url
+    }
+    projectsRef.push().set(data)
+    this.clearIinput();
+    this.getProjects();
+  }
+
+  clearIinput() {
+    this.setState({
+      title: '',
+      url: ''
+    })
+  }
+
+  removeProject = (data) => {
+    projectsRef.child(data).remove();
+  }
+
+  async getProjects() {
+    await projectsRef.on("value", snapshot => {
+      this.setState({ projectList: snapshot.val() });
+    });
+  }
+
+  loadingLists = (data) => {
+    const list = Object.keys(data);
+    
+    const lists = list.map((key, index) => { 
+      return (<span className="listing" key={index}>{data[key].title} <span className="remove" onClick={() => this.removeProject(list[index])}>x</span></span>);
+    });
+
+    return lists;
+  }
+
+  render() {
+    const { title, url, projectList } = this.state;
+
+    return (
+      <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Load Data Into Apps
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form>
+          <div className="from_group">
+            <label>Enter project title</label>
+            <input type="text" name="title" onChange={(e) => this.handleChange(e)} value={title} />
+          </div>
+          <div className="from_group">
+            <label>Enter project url</label>
+            <input type="text" name="url" onChange={(e) => this.handleChange(e)} value={url} />
+          </div>
+          <div className="from_group">
+            <button value='Submit' className="submit" onClick={(e) => this.handleSubmit(e)}>Submit</button>
+          </div>
+        </form>
+
+        <div>
+          <h3>Project Lists</h3>
+          <div>
+            {projectList && this.loadingLists(projectList)}
+            {projectList.length < 1 && <div className="loading">...loading</div>}
+          </div>
+        </div>
       </header>
     </div>
-  );
+    )
+  }
 }
-
-export default App;
