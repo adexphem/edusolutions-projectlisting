73fa65ab25de0257a0b7491ae4d29db60b08ca82
adexphem <adexphem4real@gmail.com>
2019-11-24 22:02:05 +0100

    update base and added now v2 deploy process

--- src/base.js
+++ src/base.js
@@ -1,9 +1,9 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: process.env.react_app_apikey || "",
-  authDomain: process.env.react_app_authdomain || "",
-  databaseURL: process.env.react_app_databaseurl || ""
+  apiKey: process.env.REACT_APP_API_KEY || "",
+  authDomain: process.env.REACT_APP_AUTHDOMAIN || "",
+  databaseURL: process.env.REACT_APP_DATABASEURL || ""
 };
 
 firebase.initializeApp(config);
